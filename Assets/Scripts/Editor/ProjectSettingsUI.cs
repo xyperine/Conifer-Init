e6c2b0d87e6defc84dc7000ac947da939f3d9bcc
xyperine <ewsannne@gmail.com>
2026-06-16 18:19:00 +0300

    Extracted project settings ui from SetupWindow.

---
+++ Assets/Scripts/Editor/ProjectSettingsUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ProjectSetup.Editor
+{
+    public class ProjectSettingsUI
+    {
+        private readonly SetupBusiness _business;
+
+
+        public ProjectSettingsUI(SetupBusiness business)
+        {
+            _business = business;
+        }
+        
+        
+        public void Draw()
+        {
+            GUILayout.Label("Project Settings", new GUIStyle(EditorStyles.boldLabel));
+
+            using GUILayout.VerticalScope s = new GUILayout.VerticalScope(new GUIStyle());
+
+            ProjectSettings projectSettings = _business.GetProjectSettings();
+            
+            projectSettings.DefaultNamespace =
+                EditorGUILayout.TextField("Default Namespace", projectSettings.DefaultNamespace);
+            projectSettings.GameObjectNamingScheme =
+                (EditorSettings.NamingScheme) EditorGUILayout.EnumPopup("Game Object Naming",
+                    projectSettings.GameObjectNamingScheme);
+            projectSettings.CompanyName = EditorGUILayout.TextField("Company Name", projectSettings.CompanyName);
+            projectSettings.ProductName = EditorGUILayout.TextField("Product Name", projectSettings.ProductName);
+            projectSettings.Version = EditorGUILayout.TextField("Version", projectSettings.Version);
+            projectSettings.ScriptingBackend =
+                (ScriptingImplementation) EditorGUILayout.EnumPopup("Scripting Backend", projectSettings.ScriptingBackend);
+
+            _business.SetProjectSettings(projectSettings);
+        }
+    }
+}
--- Assets/Scripts/Editor/SetupWindow.cs
+++ Assets/Scripts/Editor/SetupWindow.cs
@@ -14,19 +14,13 @@ namespace ProjectSetup.Editor
     public class SetupWindow : EditorWindow
     {
         private readonly SetupBusiness _business = new SetupBusiness();
+        
         private FolderStructureUI _folderStructureUI;
         private PackagesSettingsUI _packagesSettingsUI;
         private AssetsSettingsUI _assetsSettingsUI;
+        private ProjectSettingsUI _projectSettingsUI;
         
         private Vector2 _scrollPosition;
-        
-        // Folder structure settings
-
-
-        // Packages settings
-
-        
-        // Assets settings
         
         
         [MenuItem("Tools/Setup Window")]
@@ -45,6 +39,7 @@ namespace ProjectSetup.Editor
             _folderStructureUI = new FolderStructureUI(_business);
             _packagesSettingsUI = new PackagesSettingsUI(_business);
             _assetsSettingsUI = new AssetsSettingsUI(_business);
+            _projectSettingsUI = new ProjectSettingsUI(_business);
         }
 
 
@@ -69,7 +64,7 @@ namespace ProjectSetup.Editor
 
             SetupWindowElements.DrawRegularSpace();
             
-            DrawProjectSettings();
+            _projectSettingsUI.Draw();
             
             SetupWindowElements.DrawRegularSpace();
             
@@ -218,29 +213,6 @@ namespace ProjectSetup.Editor
             SaveAsProfile(newProfileName);
         }
         
-
-        private void DrawProjectSettings()
-        {
-            GUILayout.Label("Project Settings", new GUIStyle(EditorStyles.boldLabel));
-
-            using GUILayout.VerticalScope s = new GUILayout.VerticalScope(new GUIStyle());
-
-            ProjectSettings projectSettings = _business.GetProjectSettings();
-            
-            projectSettings.DefaultNamespace =
-                EditorGUILayout.TextField("Default Namespace", projectSettings.DefaultNamespace);
-            projectSettings.GameObjectNamingScheme =
-                (EditorSettings.NamingScheme) EditorGUILayout.EnumPopup("Game Object Naming",
-                    projectSettings.GameObjectNamingScheme);
-            projectSettings.CompanyName = EditorGUILayout.TextField("Company Name", projectSettings.CompanyName);
-            projectSettings.ProductName = EditorGUILayout.TextField("Product Name", projectSettings.ProductName);
-            projectSettings.Version = EditorGUILayout.TextField("Version", projectSettings.Version);
-            projectSettings.ScriptingBackend =
-                (ScriptingImplementation) EditorGUILayout.EnumPopup("Scripting Backend", projectSettings.ScriptingBackend);
-
-            _business.SetProjectSettings(projectSettings);
-        }
-
 
         private void DrawMiscSettings()
         {
