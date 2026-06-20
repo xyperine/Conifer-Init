309a83fabe656ba71cce1a6a00bb98a8c0eb7e40
xyperine <ewsannne@gmail.com>
2026-06-20 17:05:27 +0300

    Made AssetInfo readonly.

--- Assets/Scripts/Editor/AssetInfo.cs
+++ Assets/Scripts/Editor/AssetInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using UnityEngine;
 
 namespace ProjectSetup.Editor
 {
     /// <summary>
     /// Information about an asset in the storage.
     /// </summary>
-    [Serializable]
-    internal struct AssetInfo : IEquatable<AssetInfo>
+    internal readonly struct AssetInfo : IEquatable<AssetInfo>
     {
-        [field: SerializeField] public string Path { get; private set; }
-        [field: SerializeField] public string Name { get; private set; }
-        [field: SerializeField] public string ID { get; private set; }
+        public string Path { get; }
+        public string Name { get; }
+        public string ID { get; }
 
 
         public AssetInfo(string path, string name, string id)
