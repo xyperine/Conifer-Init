6983a17923730f65026c45d333141305a514026e
xyperine <ewsannne@gmail.com>
2026-07-04 22:10:07 +0300

    Fixed problem with the newtonsoft json dependency.

---
+++ Assets/Plugins/ConiferInit/Scripts/Initialization/Initialization.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace ConiferInit.Initialization
+{
+    [InitializeOnLoad]
+    internal static class Initialization
+    {
+        private const string NEWTONSOFT_JSON_ID = "com.unity.nuget.newtonsoft-json";
+        
+        private static readonly ListRequest ListRequest;
+        
+        
+        static Initialization()
+        {
+            ListRequest = Client.List(true);
+            
+            EditorApplication.update += Update;
+        }
+
+
+        private static void Update()
+        {
+            if (ListRequest.IsCompleted)
+            {
+                switch (ListRequest.Status)
+                {
+                    case StatusCode.InProgress:
+                        Debug.Log("Invalid request");
+                        break;
+                    case StatusCode.Success:
+                        CheckForDependencies(ListRequest.Result);
+                        break;
+                    case StatusCode.Failure:
+                        Debug.LogError(ListRequest.Error.message);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+
+        private static void CheckForDependencies(PackageCollection collection)
+        {
+            bool alreadyInstalled = false;
+            
+            foreach (PackageInfo packageInfo in collection)
+            {
+                if (packageInfo.name == NEWTONSOFT_JSON_ID)
+                {
+                    alreadyInstalled = true;
+                }
+            }
+
+            if (!alreadyInstalled)
+            {
+                InstallDependencies();
+            }
+        }
+        
+
+        private static void InstallDependencies()
+        {
+            Debug.Log("Installing dependencies...");
+            Client.Add(NEWTONSOFT_JSON_ID);
+
+            EditorApplication.update -= Update;
+        }
+    }
+}
