36c7e932be3ee89c35e3fc709547923ccc189b15
xyperine <ewsannne@gmail.com>
2026-07-21 15:26:18 +0300

    Added FolderStructureConfiguration tests.

---
+++ Assets/Plugins/ConiferInit/Scripts/Tests/Editor/FolderStructureConfigurationTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using ConiferInit.Editor.Configuration;
+using NUnit.Framework;
+
+namespace ConiferInit.Editor.Tests
+{
+    internal sealed class FolderStructureConfigurationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            ConfigurationCache.instance.Clear();
+        }
+
+
+        [Test]
+        public void Removes_folder()
+        {
+            // Arrange
+            SettingsProfileConfiguration profileConfiguration =
+                new SettingsProfileConfiguration(ConfigurationCache.instance);
+            FolderStructureConfiguration sut =
+                new FolderStructureConfiguration(profileConfiguration, ConfigurationCache.instance);
+
+            FolderStructureEntry parent = new FolderStructureEntry("Parent", (FolderStructureEntry) null);
+            FolderStructureEntry child = new FolderStructureEntry("Child", parent);
+
+            // Act
+            sut.RemoveFolder(child);
+
+            // Assert
+            Assert.IsFalse(parent.Children.Contains(child));
+        }
+        
+        
+        [Test]
+        public void Renames_folder()
+        {
+            // Arrange
+            SettingsProfileConfiguration profileConfiguration =
+                new SettingsProfileConfiguration(ConfigurationCache.instance);
+            FolderStructureConfiguration sut =
+                new FolderStructureConfiguration(profileConfiguration, ConfigurationCache.instance);
+
+            FolderStructureEntry child = new FolderStructureEntry("Test", (FolderStructureEntry) null);
+            string newName = "I am Renamed";
+            
+            // Act
+            sut.RenameFolder(child, newName);
+
+            // Assert
+            string expected = "I am Renamed";
+            Assert.AreEqual(expected, child.Name);
+        }
+        
+        
+        [Test]
+        public void Adds_folder()
+        {
+            // Arrange
+            SettingsProfileConfiguration profileConfiguration =
+                new SettingsProfileConfiguration(ConfigurationCache.instance);
+            FolderStructureConfiguration sut =
+                new FolderStructureConfiguration(profileConfiguration, ConfigurationCache.instance);
+
+            FolderStructureEntry parent = new FolderStructureEntry("Parent", (FolderStructureEntry) null);
+
+            // Act
+            sut.AddFolder("Child", parent);
+
+            // Assert
+            string expected = "Child";
+            string[] actualChildrenNames = parent.Children.Select(c => c.Name).ToArray(); 
+            Assert.Contains(expected, actualChildrenNames);
+        }
+
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            ConfigurationCache.instance.Clear();
+        }
+    }
+}
